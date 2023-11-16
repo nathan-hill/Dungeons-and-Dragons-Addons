4535f1379f2e8a3841b69bf000de589b22cdf95f
nathan-hill <50727541+nathan-hill@users.noreply.github.com>
2023-11-15 22:09:16 -0800

    Adds documentation
    Adds information on how to add the file, Adds documentation and filenames and sheet version

---
+++ acroJS/jaeger.js
@@ -0,0 +1,18 @@
+/*	-WHAT IS THIS?-
+	This file adds optional material to "MPMB's Character Record Sheet" found at https://flapkan.com/mpmb/charsheets
+	Import this file using the "Add Extra Materials" bookmark.
+	-KEEP IN MIND-
+	It is recommended to enter the code in a fresh sheet before adding any other information (i.e. before making your character with it).
+*/
+
+/*	-INFORMATION-
+	Subject:	Class
+	Effect:		This script adds the Jaeger class, a homebrew creation from Steinhardt's Guide to the Eldritch Hunt, written by MonkeyDM
+				They can be found at https://www.monkeydm.com/steinhardt-preview2
+	Code by:	u/nat1110
+	Date:		11-15-2023 (sheet v13)
+*/
+
+var iFileName = "Jaeger Class [From Steinhardt's Guide to the Eldritch Hunt, transcribed by Nat1110]js";
+
+RequiredSheetVersion("13.0.0");
